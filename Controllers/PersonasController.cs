9d7117188a617bdc1349bca1b955ec797adb4cbd
bgarcia95 <bgarcia.95@outlook.com>
2019-10-15 16:55:36 -0600

    Adding put and delete on PersonasController

--- Controllers/PersonasController.cs
+++ Controllers/PersonasController.cs
@@ -37,13 +37,39 @@ namespace RedPepper.API.Controllers
             _context.Personas.Add(persona);
             await _context.SaveChangesAsync();
 
-            return Ok(CreatedAtAction(nameof(GetPersonas), new {id = persona.Id}, persona));
+            return Ok(CreatedAtAction(nameof(GetPersons), new {id = persona.Id}, persona));
         
         /* The CreatedAtAction method:
 
             * Returns an HTTP 201 status code if successful. HTTP 201 is the standard response for an HTTP POST method that creates a new resource on the server.
             * Adds a Location header to the response. The Location header specifies the URI of the newly created to-do item.
             * References the GetPersonas action to create the Location header's URI. The C# nameof keyword is used to avoid hard-coding the action name in the CreatedAtAction call.*/           
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPerson(int id, [FromBody] Persona persona) {
+            if(id != persona.Id) {
+                return BadRequest();
+            }
+
+            _context.Entry(persona).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePerson(int id) {
+            var person = await _context.Personas.FirstOrDefaultAsync(x => x.Id == id);
+            
+            if(person == null) {
+                return NotFound("Recurso no encontrado");
+            }
+
+            _context.Personas.Remove(person);
+            await _context.SaveChangesAsync();
+            return Ok(person);
+
         }
     }
 }
